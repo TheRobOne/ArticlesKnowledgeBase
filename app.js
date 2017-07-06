e7a86b7f1757d5ee7bcf5044200c2e82262a2e03
Maciej Rusek <m_rusek@vp.pl>
2017-07-06 21:52:44 +0200

    added post method to add articles

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 //Bring in article model
 let Article = require('./models/article');
 
 const app = express();
+
+// Bring in bodyParser
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
 
 //connect do DB
 const nodekb = 'mongodb://localhost/nodekb';
@@ -13,7 +19,7 @@ mongoose.connect(nodekb);
 let db = mongoose.connection;
 
 //check for errors
-db.once('open', () => {
+db.once('openUri', () => {
   console.log("Connect to mongodb");
 });
 
@@ -31,6 +37,7 @@ app.get('/', (req, res) => {
   Article.find({}, (err, articles) => {
     if(err){
       console.log(err);
+      return;
     } else {
       res.render('index', {
         title: 'Home',
@@ -40,10 +47,27 @@ app.get('/', (req, res) => {
   });
 });
 
-//add article route
+//get method add article route
 app.get('/articles/add', (req,res) =>{
   res.render('add_article', {
     title: 'add article'
+  });
+});
+
+//post method for add article route
+app.post('/articles/add', function(req, res){
+  let article = new Article();
+  article.title = req.body.title;
+  article.author = req.body.author;
+  article.body = req.body.body;
+
+  article.save(function(err){
+    if(err){
+      console.log(err);
+      return;
+    } else {
+      res.redirect('/');
+    }
   });
 });
 
