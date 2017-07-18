5431c9c45121a445960d31874c4ed534066db69a
Maciej Rusek <m_rusek@vp.pl>
2017-07-18 23:45:57 +0200

    changed navbar for login user

--- app.js
+++ app.js
@@ -72,6 +72,11 @@ require('./config/passport')(passport);
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.get('*', (req, res, next) => {
+  res.locals.user = req.user || null;
+  next();
+});
+
 //set views
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
